8456d8fb4b34ddaa584b300106295d280e6fe74f
BenjaminCohn <Benjamin.cohen@eemi.com>
2023-09-18 20:23:49 +0200

    mmodif typeAnimation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method='POST' action="" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/7dbc6d4e-716d-4539-afd1-339192d43483" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - Benjamin.cohen@eemi.com</p>
